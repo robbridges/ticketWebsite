abe0ea052be37d071b1ac47376e9c88bba08ae1c
robbridges <robbridges@users.noreply.github.com>
2021-06-14 10:21:19 -0700

    Wiring up remote K8s cluster on gcloud

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,10 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('Hi user');
+});
+
 app.listen(3000, () => {
   console.log('listening on port 3000');
 });
