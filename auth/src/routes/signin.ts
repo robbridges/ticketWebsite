dbda97ffd886338ef640c667a32e7bf1c8494971
robbridges <robbridges@users.noreply.github.com>
2021-06-19 13:21:58 -0700

    Authentication wired up. API responses good in postman

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,58 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
+import { body  } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
+import { Password } from '../services/password';
+import { User } from '../models/user';
+
+import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req,res) => {
-  res.send('Hey user');
-});
+router.post('/api/users/signin', [
+  body('email')
+    .isEmail()
+    .withMessage('Email must be valid'),
+  body('password')
+    .trim()
+    .notEmpty()
+    .withMessage('Password cannot be empty')
+],
+validateRequest,  
+
+async (req: Request, res: Response) => {
+  const {email, password} = req.body; 
+
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) {
+    throw new BadRequestError('Invalid credentials');
+  }
+
+  const passwordsMatch = await Password.compare(
+    existingUser.password, 
+    password
+  );
+
+  if (!passwordsMatch) {
+    throw new BadRequestError('Invalid credentials');
+  }
+
+   // create a JWT, and store it in the session
+   const userJwt = jwt.sign({
+    id: existingUser.id,
+    email: existingUser.email
+  }, process.env.JWT_TOKEN!
+  );
+
+  // we have to store it as an object because typescript does not like the type definition
+  req.session = {
+    jwt: userJwt
+  };
+
+  res.status(200).send(existingUser)
+
+}
+);
 
 export { router as signinRouter };
