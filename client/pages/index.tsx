cad0e4484d39707f1a7adc26feec2a6daa9001fe
robbridges <robbridges@users.noreply.github.com>
2021-06-25 18:36:14 -0700

    Fixing typescript errors

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,10 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
+import {  NextPageContext } from 'next';
+
 
 interface serverResponse  {
-  data: currentUser
+  data?: currentUser
+  req: Request
 }
 
 
@@ -19,20 +22,40 @@ type Props = {
 // this is our landing page, it is going to try to get props passed in if a current user is signed in. So that we can then conditionally different objects
 const LandingPage = ({ currentUser } : Props) => {
   console.log(currentUser);
+  
  
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ( {req} : NextPageContext ) => {
+  // we have to do some weird type checking here to try to determine if the request is being handled internally by next Js, which will not because it needs to reach out to NGINX, or the browser
+  
   if (typeof window === 'undefined') {
-    // we are on server, make request to nginx
+    
+    try {
+      const {data} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
+        {
+            headers: req!.headers
+        }
+      );
+
+      return data;
+    } catch(err) {
+      console.log(err.message);
+    }
+    
+    // return response.data;
+
   } else {
     // we are on browser requests can be made to regular url
-    
-    const response : any  = await axios.get<currentUser>('/api/users/currentuser').catch((err) => {
+    try {
+      const response = await axios.get('/api/users/currentuser');
+      return response.data;
+    } catch(err) {
       console.log(err.message);
-    });
-    return response.data;
+    }
+
+    
     
   }
   return {};
