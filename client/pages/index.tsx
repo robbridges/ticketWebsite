b38281d21246cf692f6fe25684e60d5da4e834d5
robbridges <robbridges@users.noreply.github.com>
2021-06-26 05:07:10 -0700

    Update index.tsx

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,10 +2,6 @@ import axios from 'axios';
 import { NextPageContext } from 'next';
 
 
-
-
-
-
 interface currentUser  {
   id: string,
   email: string,
@@ -17,47 +13,12 @@ type Props = {
   currentUser :currentUser
 }
 // this is our landing page, it is going to try to get props passed in if a current user is signed in. So that we can then conditionally different objects
+
 const LandingPage = ({ currentUser }: Props) => {
   return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>;
 };
 
-// LandingPage.getInitialProps = async ( {req} : NextPageContext ) => {
-//   // we have to do some weird type checking here to try to determine if the request is being handled internally by next Js, which will not because it needs to reach out to NGINX, or the browser
-  
-//   if (typeof window === 'undefined') {
-    
-//     try {
-//       if (req) {
-//         const {data} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
-//           {   
-              
-//               headers: req.headers
-//           }
-//         );
 
-//         return data;
-//       }
-//     } catch(err) {
-//       console.log(err.message);
-//     }
-    
-    
-
-//   } else {
-//     // we are on browser requests can be made to regular url
-//     try {
-//       const response = await axios.get('/api/users/currentuser');
-//       return response.data;
-//     } catch(err) {
-//       console.log(err.message);
-//     }
-
-    
-    
-//   }
-//   return {};
-
-// };
 
 export async function getServerSideProps({ req }: NextPageContext) {
   const response = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', { headers: req!.headers });
