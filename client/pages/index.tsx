abfa608315a517c975d2537e5934c6ea00895e80
robbridges <robbridges@users.noreply.github.com>
2021-06-26 05:05:36 -0700

    Fixed old next js depreciated call. No need to refactor any longer.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {  NextPageContext } from 'next';
+import { NextPageContext } from 'next';
 
 
 
@@ -17,49 +17,59 @@ type Props = {
   currentUser :currentUser
 }
 // this is our landing page, it is going to try to get props passed in if a current user is signed in. So that we can then conditionally different objects
-const LandingPage = ({ currentUser } : Props) => {
-  console.log(currentUser);
-  
- 
-  return <h1>Landing Page</h1>;
+const LandingPage = ({ currentUser }: Props) => {
+  return currentUser ? <h1>You are signed in</h1> : <h1>You are not signed in</h1>;
 };
 
-LandingPage.getInitialProps = async ( {req} : NextPageContext ) => {
-  // we have to do some weird type checking here to try to determine if the request is being handled internally by next Js, which will not because it needs to reach out to NGINX, or the browser
+// LandingPage.getInitialProps = async ( {req} : NextPageContext ) => {
+//   // we have to do some weird type checking here to try to determine if the request is being handled internally by next Js, which will not because it needs to reach out to NGINX, or the browser
   
-  if (typeof window === 'undefined') {
+//   if (typeof window === 'undefined') {
     
-    try {
-      if (req) {
-        const {data} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
-          {   
+//     try {
+//       if (req) {
+//         const {data} = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', 
+//           {   
               
-              headers: req.headers
-          }
-        );
+//               headers: req.headers
+//           }
+//         );
 
-        return data;
-      }
-    } catch(err) {
-      console.log(err.message);
+//         return data;
+//       }
+//     } catch(err) {
+//       console.log(err.message);
+//     }
+    
+    
+
+//   } else {
+//     // we are on browser requests can be made to regular url
+//     try {
+//       const response = await axios.get('/api/users/currentuser');
+//       return response.data;
+//     } catch(err) {
+//       console.log(err.message);
+//     }
+
+    
+    
+//   }
+//   return {};
+
+// };
+
+export async function getServerSideProps({ req }: NextPageContext) {
+  const response = await axios.get('http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', { headers: req!.headers });
+  return {
+    props: {
+      currentUser: response.data,
     }
-    
-    
-
-  } else {
-    // we are on browser requests can be made to regular url
-    try {
-      const response = await axios.get('/api/users/currentuser');
-      return response.data;
-    } catch(err) {
-      console.log(err.message);
-    }
-
-    
-    
   }
-  return {};
+} 
 
-};
 
-export default LandingPage;
+
+
+
+export default LandingPage; 
