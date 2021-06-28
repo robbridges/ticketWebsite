dc666f333b0dac99c611e34793b13017929b9884
robbridges <robbridges@users.noreply.github.com>
2021-06-28 13:10:42 -0700

    finished up testing and middleware wire in of the new ticket route. Spun up Mongo DB ticket Schema. Everything seems to work

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from 'express';
 import { requireAuth, validateRequest } from '@ticket.dev/common';
 import { body } from 'express-validator';
+import { Ticket } from '../models/tickets';
 
 const router = express.Router();
 
@@ -14,8 +15,18 @@ router.post('/api/tickets', requireAuth, [
     .withMessage('Price must be greater than 0')
 
 
-], validateRequest, (req:Request, res: Response) => {
-  res.sendStatus(200);
+], validateRequest, async (req:Request, res: Response) => {
+  const {title, price} = req.body;
+
+  const ticket= Ticket.build({
+    title,
+    price,
+    userId: req.currentUser!.id
+  });
+
+  await ticket.save();
+  
+  res.status(201).send(ticket);
 });
 
 export { router as createTicketRouter };
