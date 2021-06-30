8a3766dec17eea30a0eb710878d3cf2d3fb0a6a7
robbridges <robbridges@users.noreply.github.com>
2021-06-29 19:49:14 -0700

    NATS Streaming service made, we are making a small NATS test project

--- common/src/middlewares/validate-request.ts
+++ common/src/middlewares/validate-request.ts
@@ -2,6 +2,7 @@ import {Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
 import { RequestValidationError } from '../errors/request-validation-error';
 
+// this is our request validator, if any of the requests requirements outlined in our mongoose model do not confirm this will collect the error.
 export const validateRequest = (
   req: Request, 
   res: Response, 
