91f7a4d109061f824a9a6152ec563b44d9d4ec5d
robbridges <robbridges@users.noreply.github.com>
2021-07-03 12:00:50 -0700

    More comments

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -5,7 +5,8 @@ const stan = nats.connect('ticketing', 'abc', {
 });
 
 
-
+/* we are sending over the fake data object just to practice sending through nats nats actually does not let objects be sent to it
+it wants single strings, so we're going to stringify the object to make nats happy */
 stan.on('connect', () => {
   console.log('Publisher connected to nats');
 
