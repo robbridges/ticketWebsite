62728195b45ddc32527227751e1f626b8b3f9ea8
robbridges <robbridges@users.noreply.github.com>
2021-07-03 13:05:28 -0700

    Refactoring abstract listener class out for better code readability

---
+++ nats-test/src/events/base-listener.ts
@@ -0,0 +1,56 @@
+import {Message, Stan } from 'node-nats-streaming'
+
+export abstract class Listener {
+  abstract subject: string;
+  abstract queueGroupName: string;
+  abstract onMessage(data: any, msg: Message): void;
+  private client: Stan;
+  protected ackWait = 5 * 1000;
+  
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  /* 
+  chaining different options to nats, manual acknowledgements so that we only complete
+  a request once it's been succesful, and playing with delivery all mode, though this not forseeable as
+  when any project scales we are looking at resending a lot of events at once.
+  
+  Fixed! We can set a durable name a que group so that nats keeps a record and sends us what we need. 
+  So then we do not send the entire list to the group once it restarts
+  */
+  subscriptionOptions() {
+    return this.client
+    .subscriptionOptions()
+    .setDeliverAllAvailable()
+    .setManualAckMode(true)
+    .setAckWait(this.ackWait)
+    .setDurableName(this.queueGroupName)
+
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on('message', (msg: Message) => {
+      console.log(
+        `Message recieved ${this.subject} / ${this.queueGroupName}`
+      );
+
+      const parseData = this.parseMessage(msg);
+      this.onMessage(parseData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+      const data = msg.getData();
+      return typeof data ==='string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8'));
+  }
+}
--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,6 +1,7 @@
 
-import nats, {Message} from 'node-nats-streaming'
+import nats from 'node-nats-streaming'
 import { randomBytes } from 'crypto';
+import { TicketCreatedListener} from './events/ticket-created-listener';
 
 
 console.clear();
@@ -17,37 +18,14 @@ stan.on('connect', () => {
     console.log('NATS connection closed!');
     process.exit();
   });
-  /* 
-  chaining different options to nats, manual acknowledgements so that we only complete
-  a request once it's been succesful, and playing with delivery all mode, though this not forseeable as
-  when any project scales we are looking at resending a lot of events at once.
   
-  Fixed! We can set a durable name a que group so that nats keeps a record and sends us what we need. 
-  So then we do not send the entire list to the group once it restarts
-  */
-  const options = stan
-  .subscriptionOptions()
-  .setManualAckMode(true)
-  .setDeliverAllAvailable()
-  .setDurableName('order-service');
-
-  const subscription = stan.subscribe(
-    'ticket:created', 
-    'listenerQueueGroup',
-    options
-  );
-
-  subscription.on('message', (msg : Message ) => {
-    const data = msg.getData();
-
-    if (typeof data === 'string') {
-      console.log(`Recieved event #${msg.getSequence()}, with data: ${(data)}`)
-    }
-
-    msg.ack();
-  });
+  new TicketCreatedListener(stan).listen();
 });
 
 
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
+
+
+
+
