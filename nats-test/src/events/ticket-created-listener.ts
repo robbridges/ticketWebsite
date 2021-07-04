d6f81550359013dd4240546243d51b23a55de691
robbridges <robbridges@users.noreply.github.com>
2021-07-03 21:44:24 -0700

    Wiring up better typescript in base listener and nats listener files

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,10 +1,13 @@
 import { Listener } from './base-listener';
 import { Message } from 'node-nats-streaming'
-
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
+// we created a TS interface to help us make sure our subject and dat are correct, since there will be so many this well help. 
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject : Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payments-service'
-  onMessage(data: any, msg: Message) {
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Event data!', data)
 
     msg.ack();
