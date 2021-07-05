5a060ec101254e089f920c5e2903ca07f7c6adad
robbridges <robbridges@users.noreply.github.com>
2021-07-04 19:21:59 -0700

    Testing that events can be published in real project, not test one. Successfully

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, {Request, Response} from 'express';
 import { requireAuth, validateRequest } from '@ticket.dev/common';
 import { body } from 'express-validator';
 import { Ticket } from '../models/tickets';
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 // route handler to create new ticket
@@ -25,6 +27,13 @@ router.post('/api/tickets', requireAuth, [
   });
 
   await ticket.save();
+
+  await new TicketCreatedPublisher(natsWrapper.client).publish({
+    id: ticket.id,
+    title: ticket.title,
+    price: ticket.price,
+    userId: ticket.userId
+  });
   
   res.status(201).send(ticket);
 });
