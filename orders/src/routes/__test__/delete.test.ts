d360561581a841c395cf80e940071fdd072794da
robbridges <robbridges@users.noreply.github.com>
2021-07-08 16:13:09 -0700

    Most tests for order service complete, need to wire up events first before finishing last 2 tests

---
+++ orders/src/routes/__test__/delete.test.ts
@@ -0,0 +1,40 @@
+import request from 'supertest';
+import { app } from '../../app';
+import { Ticket } from '../../models/ticket';
+import { Order, OrderStatus } from '../../models/order';
+
+
+
+it('marks an order as cancelled', async () => {
+  //create ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20
+  });
+  await ticket.save();
+
+  const user = global.signin();
+
+  //make request to create  order
+  const {body : order} = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ticketId: ticket.id})
+    .expect(201);
+
+  // make request to cancel Order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204);
+
+
+
+  //expect to make sure order status is cancelled
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+});
+
+it.todo('emits a order cancelled event');
