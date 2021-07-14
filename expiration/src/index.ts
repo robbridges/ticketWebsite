f698a72515a57d5673b93b5325fb3271904d7534
robbridges <robbridges@users.noreply.github.com>
2021-07-13 20:44:09 -0700

    Wiring up expiration service, simple service really, not even an express server or Mongo DB service with it. Just listens and emits events

---
+++ expiration/src/index.ts
@@ -0,0 +1,42 @@
+
+import { natsWrapper } from './nats-wrapper';
+
+
+const start = async () => {
+  
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error ('NATS client ID must be defined');
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error ('NATS url must be defined');
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error ('NATS cluster ID must be defined');
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    // graceful shut down on nats client, so that nats knows to stop looking for something we just shut down.
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+    process.on('SIGINT', () => natsWrapper.client.close());
+    process.on('SIGTERM', () => natsWrapper.client.close());
+
+    
+    
+  } catch (err) {
+    console.error(err);
+  }
+  
+};
+
+start();
+
+
+
