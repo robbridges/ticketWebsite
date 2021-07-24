5780f6b35cccfa92397fa564a418b2d40da9ac8b
robbridges <robbridges@users.noreply.github.com>
2021-07-23 20:38:28 -0700

    Wired up ticket creation in NextJS app. Working great. Moving on to showing all tickets

--- client/pages/tickets/new.tsx
+++ client/pages/tickets/new.tsx
@@ -1,8 +1,25 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/useRequest';
 
 const NewTicket = () => {
   const [title, setTitle] = useState('');
   const [price, setPrice] = useState('');
+  const {doRequest, errors} = useRequest({
+    url:'/api/tickets',
+    method: 'post',
+    body: {
+      title,
+      price,
+    },
+    onSuccess: () => Router.push('/'),
+  });
+
+  const onSubmit = async (event : React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await doRequest();
+  }
 
   const onBlur = () => {
     const value = parseFloat(price);
@@ -15,9 +32,20 @@ const NewTicket = () => {
   }
  
   return (
+    
+
     <div>
       <h1>Create a ticket</h1>
-      <form>
+      <form onSubmit={onSubmit}>
+        {  errors.length > 0 && (
+          <div className="alert alert-danger">
+          <h4>Validation error occured</h4>
+          <ul className="my-0">
+            {errors.map(err => (<li key={err.message}>{err.message}</li>))}
+          </ul>
+          </div>)
+        }
+
         <div className="form-group">
           <label>Title</label>
           <input 
@@ -34,10 +62,10 @@ const NewTicket = () => {
           onChange={e => setPrice(e.target.value)} 
           className="form-control" />
         </div>
-        
+        <button className="btn btn-primary">Submit</button>  
       </form>
-      <br />
-      <button className="btn btn-primary">Submit</button>
+      
+      
     </div>
   )
 };
